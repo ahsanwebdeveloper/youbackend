f8d3dad39ec98bf816cd02f1c6f6b355c51ab6af
ahsanwebdeveloper <ahsanaliecomrice@gmail.com>
2025-06-19 10:08:32 -0700

    custom api response error handler

---
+++ src/utils/apiErrorhandler.js
@@ -0,0 +1,23 @@
+class apiError extends Error{
+    constructor(
+        statuscode,
+        message="something went wrong",
+        errors=[],
+        statck="",
+        
+    ){
+        super(message)
+        this.statuscode=statuscode;
+        this.data=null;
+        this.message=message;
+        this.success= false;
+        this.errors=this.errors;
+        if(statck){
+            this.statck=statck
+        }else{
+            Error.captureStackTrace(this,this.constructor)
+            
+        }
+    }
+}
+export {apiError}
