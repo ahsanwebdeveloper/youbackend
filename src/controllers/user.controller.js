6d75b53ae458093a63dc3971d36fc096a9f657bb
ahsanwebdeveloper <ahsanaliecomrice@gmail.com>
2025-06-25 07:27:01 -0700

    create user register

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,17 @@
 import { asyncHendler } from "../utils/anyscHandler.js";
 
 const registeruser = asyncHendler(async (req, res) => {
-    res.status(200).json({
-        message: "Api is working"
-    });
+    //get user detail from frontend
+    //validation -not empty
+    //check if user already exists: username, email
+    //check for image ,check for avatar
+    //upload them to cloudinary, avatar
+    //create user object -create entery in db
+    //remove password and refresh token field from response 
+    //check for user creation 
+    // return response
+    const {fullname,email,username,password}=req.body
+    console.log("email:",email)
 });
 
 export { registeruser };
